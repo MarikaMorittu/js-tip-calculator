4544fd354d5115745baa030564e84843a221ffeb
MarikaMorittu <marikaegio@hotmail.it>
2022-06-07 22:27:37 +0200

    tip-calculator

--- main.js
+++ main.js
@@ -4,26 +4,30 @@ const inputCustom = document.querySelector(".input__custom");
 const tipOut = document.querySelector(".tipOut");
 const totalOut = document.querySelector(".totalOut");
 const buttonReset = document.querySelector(".button__reset");
-const numberError = document.querySelector(".number");
+const error = document.querySelector(".error");
+const boxPeople = document.querySelector(".people__box")
 let boxTip = document.querySelectorAll(".percent");
 
 
 
 boxTip.forEach((element) => {
   element.addEventListener("click", () => {
-    errorZero( inputPeople.value)
+    errorZero(inputPeople.value);
     calculatorBill(inputBill.value, inputPeople.value, totalOut);
     calculatorTip(inputBill.value, element, inputPeople.value, tipOut);
   });
 });
 // funzione per carcolare totalOut
-function calculatorBill(bill, people, totalOut) {
+function calculatorBill(bill, people, totalOut, tipOut) {
   let resultBill = (parseFloat(bill) / parseFloat(people));
-  if( resultBill != "Infinity"){ 
-    totalOut.innerHTML = resultBill.toFixed(2);
+  // controllo 
+  if(isNaN(resultBill)){ 
+    tipOut.innerHTML ="0.00";
+    totalOut.innerHTML= "0.00"
   }
-  else{
-    totalOut.innerHTML= "0.00" 
+  if(resultBill != "Infinity"){
+    totalOut.innerHTML = resultBill.toFixed(2);
+    
   }
 }
 
@@ -34,9 +38,10 @@ function calculatorTip(bill, boxTip, people, tipOut, totalOut) {
     if (boxTip.classList.contains(`${tips[i]}`)) {
       let resultMount = ((parseFloat(bill) / parseFloat(people)) * `${tips[i]}`) / 100;
       tipOut.innerHTML = resultMount.toFixed(2);
+       // controllo 
       if(isNaN(resultMount) || resultMount == "Infinity" ){
         tipOut.innerHTML ="0.00";
-        totalOut.innerHTML= "0.00"
+        totalOut.innerHTML= "0.00";
       }
     }
   }
@@ -49,11 +54,23 @@ function customTip( bill, inputCustom, people, tipOut){
 // funzione per calcolare tipOut e totalOut con il custom
 inputCustom.addEventListener("keypress", (e)=>{
   if(e.key === "Enter"){
+    errorZero(inputPeople.value);
     customTip(inputBill.value, inputCustom.value, inputPeople.value, tipOut)
     calculatorBill(inputBill.value, inputPeople.value, totalOut);
   }
 
 })
+// funzione per far aparire il messaggio di errore
+function errorZero(people){
+  if(people == 0){
+    error.classList.add("block")
+    boxPeople.classList.add("border")
+  }else{
+   error.classList.remove("block")
+   boxPeople.classList.remove("border")
+  }
+}
+
 // funzione di Reset
 function resetAll(bill, people,inputCustom, tipOut, totalOut){
   buttonReset.addEventListener("click",() =>{
@@ -68,10 +85,5 @@ function resetAll(bill, people,inputCustom, tipOut, totalOut){
 resetAll(inputBill,inputPeople,inputCustom,tipOut,totalOut)
 
 
-function errorZero(inputPeople){
-  if(inputPeople <= 0){
-    let error = `<span class="cant-be-zero">can't be zero</span>`;
-    numberError.innerHTML += error; 
-  }
-}
+
 
